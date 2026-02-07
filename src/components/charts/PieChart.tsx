08638f3d67aeb1bb135d945267a3288cce29df8a
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2026-02-07 23:44:56 +0500

    set the piechart in doctorDashboard

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,18 +1,41 @@
 import type { ApexOptions } from 'apexcharts';
 import React from 'react'
+import ReactApexChart from 'react-apexcharts';
 
-const PieChart:React.FC = () => {
-    const series=[40,50,60];
+const PieChartComp: React.FC = () => {
+  const series = [40, 50, 60,5];
 
-    const options:ApexOptions={
-        chart:{
-            type:'pie',
+  const options: ApexOptions = {
+    chart: {
+      type: 'pie',
+    },
+    labels: ['Completed Visits', 'Upcoming Visits', 'Cancelled Visits', 'First-Time Patients'],
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          chart: {
+            width: 280,
+          },
+          legend:{
+            position:'bottom',
+          },
         },
-        labels:[]
+
+      }
+    ],
+    legend:{
+      position:'right',
+    },
+    dataLabels:{
+      enabled:true,
     }
+  }
   return (
-    <div>PieChart</div>
+    <div>
+      <ReactApexChart options={options} series={series} type="pie" height={350} />
+    </div>
   )
 }
 
-export default PieChart
+export default PieChartComp
--- src/features/doctor/DoctorDashboard.tsx
+++ src/features/doctor/DoctorDashboard.tsx
@@ -1,13 +1,22 @@
 // import React from 'react'
 
+import PieChartComp from "../../components/charts/PieChart"
 import StatisticComp from "../../components/Statistic"
 
 const DoctorDashboard = () => {
   return (
     <>
-    <div className="statistic-container">
-      <StatisticComp />
-    </div>
+      <div className="statistic-container">
+        <StatisticComp />
+      </div>
+      <div className="chart-container w-full grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+        <div className="current-progress-container border-2">
+          <PieChartComp />
+        </div>
+        <div className="previous-progress-container border-2">
+          <PieChartComp />
+        </div>
+      </div>
     </>
   )
 }
